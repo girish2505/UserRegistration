18fcda44f53897bff5a126b3c568351deff1aa11
Vayugundla Girish Guptha <girishvayugundla@gmail.com>
2021-07-15 18:44:10 +0530

    Validating first name for user registration

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----> User Registration Problem <-----");
+            RegExp regExp = new RegExp();
+            regExp.ValidateFirstName();
+        }
+    }
+}
---
+++ UserRegistrationProblem/RegExp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace UserRegistrationProblem
+{
+    class RegExp
+    {
+        public static string s = "^[A-Z][a-z]{2,}$";
+        public void ValidateFirstName()
+        {
+            Regex exp = new Regex(s);
+            Console.WriteLine("Enter first name :");
+            string firstName = Console.ReadLine();
+            bool result = Regex.IsMatch(firstName, s);
+            if (result)
+            {
+                Console.WriteLine("Valid name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name");
+                Console.WriteLine("First name starts with Cap and has minimum 3 characters");
+            }
+        }
+    }
+}
